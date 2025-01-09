3a6e44899b11d328edc65b06f14c5c0d5e1d6deb
Tiago Fialho <tiago.chem@gmail.com>
2025-01-09 06:47:30 -0300

    exercicio 04 resolvido

--- exercicios.py
+++ exercicios.py
@@ -23,43 +23,57 @@
 # EXERCICIO 03
 # Você está analisando logs de uma aplicação e precisa filtrar mensagens com severidade 'ERROR'. Dado um registro de log em formato de dicionário como log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}, escreva um programa que imprima a mensagem se a severidade for 'ERROR'.
 
-log = {
-    'timestamp': '2021-06-23 10:00:00', 
-    'level': 'ERROR',
-    'message': 'Falha na conexão'
-}
+# log = {
+#     'timestamp': '2021-06-23 10:00:00', 
+#     'level': 'ERROR',
+#     'message': 'Falha na conexão'
+# }
 
-lista_logs = [
-    {
-        'timestamp': '2021-06-23 10:00:00', 
-        'level': 'ERROR',
-        'message': 'Falha na conexão'
-    },
-    {
-        'timestamp': '2021-06-23 10:00:00', 
-        'level': 'NORMAL',
-        'message': 'Conexão estabelecida'
-    },
-    {
-        'timestamp': '2021-06-23 11:00:00', 
-        'level': 'ERROR',
-        'message': 'Banco de dados inválido'
-    },
-    {
-        'timestamp': '2021-06-23 10:00:00', 
-        'level': 'ERROR',
-        'message': 'Falha na conexão'
-    }
-]
+# lista_logs = [
+#     {
+#         'timestamp': '2021-06-23 10:00:00', 
+#         'level': 'ERROR',
+#         'message': 'Falha na conexão'
+#     },
+#     {
+#         'timestamp': '2021-06-23 10:00:00', 
+#         'level': 'NORMAL',
+#         'message': 'Conexão estabelecida'
+#     },
+#     {
+#         'timestamp': '2021-06-23 11:00:00', 
+#         'level': 'ERROR',
+#         'message': 'Banco de dados inválido'
+#     },
+#     {
+#         'timestamp': '2021-06-23 10:00:00', 
+#         'level': 'ERROR',
+#         'message': 'Falha na conexão'
+#     }
+# ]
 
-# if log['level'] == 'ERROR':
-#     print(log['message'])
+# # if log['level'] == 'ERROR':
+# #     print(log['message'])
 
-contador_erros = 0
+# contador_erros = 0
 
-for log in lista_logs:
-    if log['level'] == 'ERROR':
-        contador_erros +=1
-        print(f"Em {log['timestamp']}: {log['message']}")
+# for log in lista_logs:
+#     if log['level'] == 'ERROR':
+#         contador_erros +=1
+#         print(f"Em {log['timestamp']}: {log['message']}")
 
-print(f"O total de erros encontrados foi de {contador_erros}.")
+# print(f"O total de erros encontrados foi de {contador_erros}.")
+
+
+# EXERCICIO 04
+# você precisa garantir que cada usuário tenha idade entre 18 e 65 anos e tenha fornecido um email válido. Escreva um programa que valide essas condições e imprima "Dados de usuário válidos" ou o erro específico encontrado.
+
+idade = int(input("Idade: "))
+email = input("E-mail: ").strip().lower()
+
+if idade < 18 or idade > 65:
+    print("Faixa etária não admitida")
+elif "@" not in email or "." not in email:
+    print("E-mail inválido")
+else:
+    print("Dados válidos!")
