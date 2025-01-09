8bdd80a296b8fd262cc2d8ed9e5a9b5143e478d8
Tiago Fialho <tiago.chem@gmail.com>
2025-01-09 05:59:06 -0300

    exercicio 01 resolvido

---
+++ exercicios.py
@@ -0,0 +1,10 @@
+# EXERCICIO 01
+quantidade = int(input("Quantidade: "))
+preco = float(input("preco: "))
+
+if quantidade > 0 and preco >= 0:
+    print("Dados válidos!")
+else:
+    print("Dados inválidos")
+
+    
