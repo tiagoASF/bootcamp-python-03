ffecb84badde49099ca25fef70288c6821e880f2
Tiago Fialho <tiago.chem@gmail.com>
2025-01-09 06:03:46 -0300

    exercicio 02 resolvido

--- exercicios.py
+++ exercicios.py
@@ -1,10 +1,20 @@
 # EXERCICIO 01
-quantidade = int(input("Quantidade: "))
-preco = float(input("preco: "))
+# quantidade = int(input("Quantidade: "))
+# preco = float(input("preco: "))
 
-if quantidade > 0 and preco >= 0:
-    print("Dados válidos!")
+# if quantidade > 0 and preco >= 0:
+#     print("Dados válidos!")
+# else:
+#     print("Dados inválidos")
+
+# EXERCICIO 02
+# Temperatura < 18°C é 'Baixa'
+# Temperatura >= 18°C e <= 26°C é 'Normal'
+# Temperatura > 26°C é 'Alta'
+temperatura_atual = float(input("Temperatura: "))
+if temperatura_atual < 18:
+    print(f"{temperatura_atual}o é BAIXA")
+elif temperatura_atual >= 18 and temperatura_atual <= 26:
+    print(f"{temperatura_atual} é NORMAL")
 else:
-    print("Dados inválidos")
-
-    
+    print(f"{temperatura_atual} é ALTA")
